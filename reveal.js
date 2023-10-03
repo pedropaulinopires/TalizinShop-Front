b645f9c89e71c342d9e248c14cef26e86a0fec7d
Pedro Henrique Paulino Pires <121137670+pedropaulinopires@users.noreply.github.com>
2023-10-02 21:51:42 -0300

    save all

--- reveal.js
+++ reveal.js
@@ -6,3 +6,12 @@ ScrollReveal().reveal('.card', {
     reset: false,
     scale:1.1
  });
+
+ 
+ ScrollReveal().reveal('.icons', { 
+    easing:"ease-out",
+    interval:80,
+    reset: true,
+    distance: '50%',
+    origin: 'bottom',
+ });
